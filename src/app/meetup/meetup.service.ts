f8df879158161e01af6346a3371b463a3c6f7e92
gilcreque <me@gilcreque.com>
2018-07-06 15:31:35 -0400

    Added upcming events

--- src/app/meetup/meetup.service.ts
+++ src/app/meetup/meetup.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetupService {
 
-  constructor(private  httpClient:  HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
+
+  API_URL  =  'https://api.meetup.com';
+  API_KEY =   '5c483b7f507144933403e5d4311e48';
+  UPCOMING_EVENTS = '/find/upcoming_events?&sign=true&photo-host=secure&topic_category=34';
+
+  getUpcomingEvents( lat: number , lon: number , radius: number ) {
+    let request_url = this.API_URL + this.UPCOMING_EVENTS + '&lat=' + lat + '&lon=' + lon + '&key=' +  this.API_KEY;
+    return this.httpClient.get(request_url);
+  }
 
 }
