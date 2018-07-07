f799ffd21fbf10c9ed35cd1ad93a10a3839d8b25
gilcreque <me@gilcreque.com>
2018-07-07 16:25:10 -0400

    Switched event layout to use cards in flex grid

--- src/app/meetup/meetup.service.ts
+++ src/app/meetup/meetup.service.ts
@@ -12,7 +12,7 @@ export class MeetupService {
 
   API_URL  =  'https://api.meetup.com';
   API_KEY =   '5c483b7f507144933403e5d4311e48';
-  UPCOMING_EVENTS = '/find/upcoming_events?&sign=true&photo-host=secure&topic_category=34&callback=JSONP_CALLBACK';
+  UPCOMING_EVENTS = '/find/upcoming_events?&sign=true&photo-host=secure&topic_category=34&fields=featured_photo&callback=JSONP_CALLBACK';
 
   getUpcomingEvents( lat: number , lon: number , radius: number ) {
     const request_url = this.API_URL + this.UPCOMING_EVENTS + '&lat=' + lat + '&lon=' + lon + '&key=' +  this.API_KEY;
