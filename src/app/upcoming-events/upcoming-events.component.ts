ede9f46f4e09ed9b9609fc8c1c8e74e425c1b888
gilcreque <me@gilcreque.com>
2018-10-04 20:44:38 -0400

    Fixed event router issue with it not being public

--- src/app/upcoming-events/upcoming-events.component.ts
+++ src/app/upcoming-events/upcoming-events.component.ts
@@ -13,10 +13,12 @@ export class UpcomingEventsComponent implements OnInit {
   public events: Array<object> = [];
   public isCollapsed = false;
   private city: Array<object> = [];
+  public routerURL;
   constructor(private meetupService: MeetupService, private router: Router) { }
 
   ngOnInit() {
     this.getUpcomingEvents ( 28.07, -80.63, 15 );
+    this.routerURL = this.router.url;
   }
   public getUpcomingEvents(lat: number, lon: number, radius: number) {
     this.meetupService.getUpcomingEvents(lat, lon, radius).subscribe((data: Array<object>) => {
