7b2e510bcd517efcbc2d880ea512a5004b85f0a6
gilcreque <me@gilcreque.com>
2018-10-05 23:37:32 -0400

    Replaced header with logo and restructured header section

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,21 +9,6 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    window.onload = function() {
-      const navbarimg = <HTMLCanvasElement>document.getElementById('navbarimg');
-      const mainsection = document.getElementById('main');
-      mainsection.style.marginTop = (navbarimg.height * 1.25) + 'px' ;
-      mainsection.style.paddingTop = '10px' ;
-
-      window.onscroll = function() {
-        if (window.pageYOffset > 0 && window.innerWidth > 1024) {
-          navbarimg.style.width = '25%';
-        } else {
-          navbarimg.style.width = '100%';
-        }
-      };
-  };
-
   }
 
 }
