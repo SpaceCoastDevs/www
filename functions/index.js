7fb34853a5317c1bdf37c342517e0d8816be0a18
gilcreque <me@gilcreque.com>
2018-10-23 21:54:46 -0400

    Created addUpomingTechEvents function

--- functions/index.js
+++ functions/index.js
@@ -4,48 +4,42 @@ const request = require('request');
 
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+API_URL = 'https://api.meetup.com';
+API_KEY = '5c483b7f507144933403e5d4311e48';
+GROUP_IDS_TO_EXCLUDE = '27306031,25219234,26307553,25328430';
+GROUP_IDS_TO_INCLUDE = '5292112';
+LAT = 28.07;
+LON = -80.63;
 
-exports.testEvents = functions.https.onRequest((req, res) => {
 
-    API_URL  =  'https://api.meetup.com';
-    API_KEY =   '5c483b7f507144933403e5d4311e48';
-    GROUP_IDS_TO_EXCLUDE = '27306031,25219234,26307553,25328430';
-    GROUP_IDS_TO_INCLUDE = '5292112';
+exports.addUpcomingTechEvents = functions.https.onRequest((req, res) => {
+
+
     UPCOMING_EVENTS = `/find/upcoming_events?&sign=true&photo-host=secure&excluded_groups=${GROUP_IDS_TO_EXCLUDE}&topic_category=292&order=time&fields=featured_photo,group_category,group_key_photo&self_groups=include`;
-//&callback=JSONP_CALLBACK
+    //&callback=JSONP_CALLBACK
 
-    const request_url = `${API_URL}${UPCOMING_EVENTS}&lat=28.07&lon=-80.63&key=${API_KEY}`;
-    const upcomingEvents = request(`${request_url}`, { json: true }, (err, res, body) => {
+    let request_url = `${API_URL}${UPCOMING_EVENTS}&lat=${LAT}&lon=${LON}&key=${API_KEY}`;
+    let upcomingTechEvents = request(`${request_url}`, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
-        const events = JSON.stringify(body['events']);
-        //console.log(events);
-        //events.forEach(event => {
-        for (event in events) {
-            console.log(event);
-            // if (typeof nestedContent === "object") {
-            //     Object.keys(nestedContent).forEach(docTitle => {
-            //         admin.firestore()
-            //             .collection('meetup-events')
-            //             .doc(nestedContent.id)
-            //             .set(nestedContent)
-            //             .then((res) => {
-            //                 return console.log("Document successfully written!");
-            //             })
-            //             .catch((error) => {
-            //                 return console.error("Error writing document: ", error);
-            //             });
-            // //     });
-            // }
-            // else {
-            //     return console.log("Not an object");
-            // }
+        // const events = JSON.stringify(body['events']);
+        const events = body['events'];
+        if (typeof events === "object") {
+            Object.keys(events).forEach(event => {
+                console.log(events[event].id);
+                admin.firestore()
+                    .collection('meetup-events')
+                    .doc(events[event].id)
+                    .set(events[event])
+                    .then((res) => {
+                        return console.log("Document successfully written!");
+                    })
+                    .catch((error) => {
+                        return console.error("Error writing document: ", error);
+                    });
+            })
         }
-        return console.log("");
+        else {
+            return console.log("events not an object");
+        }
     });
 });
