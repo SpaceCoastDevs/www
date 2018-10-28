2bdbf1d9d72feb244610cbb0d2ec8eca58f1ae56
gilcreque <me@gilcreque.com>
2018-10-27 23:37:38 -0400

    Updated firebase tools and got addUpcomingMeetupTechEvents to retrieve group IDs

--- functions/index.js
+++ functions/index.js
@@ -6,60 +6,20 @@ admin.initializeApp(functions.config().firebase);
 
 API_URL = 'https://api.meetup.com';
 API_KEY = '5c483b7f507144933403e5d4311e48';
-GROUP_IDS_TO_EXCLUDE = '27306031,25219234,26307553,25328430';
-GROUP_IDS_TO_INCLUDE = '5292112';
+CATEGORY_IDS_TO_INCLUDE = '34';
+ZIP_CODES_TO_INCLUDE = '32901,32922,32780';
+SEARCH_RADIUS = 20;
+GROUP_IDS_TO_EXCLUDE = '27306031,25219234,26307553,25328430'; //Unrelated or outside of Brevard County
+GROUP_IDS_TO_INCLUDE = [5292112]; //Groundswell to start
 LAT = 28.07;
 LON = -80.63;
+FIND_GROUP_IDS_URL = `${API_URL}/find/groups?photo-host=secure&zip=${ZIP_CODES_TO_INCLUDE}&page=100&radius=${SEARCH_RADIUS}&category=${CATEGORY_IDS_TO_INCLUDE}&key=${API_KEY}`;
 
 
 exports.addUpcomingMeetupTechEvents = functions.https.onRequest((req, res) => {
-    UPCOMING_EVENTS = `/find/upcoming_events?&sign=true&photo-host=secure&excluded_groups=${GROUP_IDS_TO_EXCLUDE}&topic_category=292&order=time&fields=featured_photo,group_category,group_key_photo&self_groups=include`;
-
-    let request_url = `${API_URL}${UPCOMING_EVENTS}&lat=${LAT}&lon=${LON}&key=${API_KEY}`;
-    let upcomingMeetupTechEvents = request(`${request_url}`, { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        const events = body['events'];
-        if (typeof events === "object") {
-            Object.keys(events).forEach(event => {
-                console.log(events[event].id);
-                admin.firestore()
-                    .collection('meetup-events')
-                    .doc(events[event].id)
-                    .set(events[event])
-                    .then((res) => {
-                        return console.log("Document successfully written!");
-                    })
-                    .catch((error) => {
-                        return console.error("Error writing document: ", error);
-                    });
-            })
-        }
-        return console.log("addUpcomingMeetupTechEvents Done");
-    });
-});
-
-exports.addUpcomingGroundswellMeetupEvents = functions.https.onRequest((req, res) => {
-    UPCOMING_GROUNDSWELL_EVENTS = `/2/events?offset=0&format=json&limited_events=False&group_urlname=StartupSpaceCoast&photo-host=secure&page=500&fields=&order=time&desc=false&status=upcoming`;
-
-    let request_url = `${API_URL}${UPCOMING_GROUNDSWELL_EVENTS}&key=${API_KEY}`;
-    let upcomingGroundswellMeetupEvents = request(`${request_url}`, { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        const events = body.results;
-        if (typeof events === "object") {
-            Object.keys(events).forEach(event => {
-                console.log(events[event].id);
-                admin.firestore()
-                    .collection('groundswelll-meetup-events')
-                    .doc(events[event].id)
-                    .set(events[event])
-                    .then((res) => {
-                        return console.log("Document successfully written!");
-                    })
-                    .catch((error) => {
-                        return console.error("Error writing document: ", error);
-                    });
-            })
-        }
-        return console.log("addUpcomingGroundswellMeetupEvents Done");
-    });
+  let getGroupIDs = request(`${FIND_GROUP_IDS_URL}`, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err);}
+    GROUP_IDS_TO_INCLUDE = GROUP_IDS_TO_INCLUDE.concat(body.map(group => group.id));
+  });
+  console.log(GROUP_IDS_TO_INCLUDE);
 });
