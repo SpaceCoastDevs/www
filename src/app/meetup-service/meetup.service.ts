c46d4aebb259e5ea225625f4ec7123f65eb042c5
gilcreque <me@gilcreque.com>
2018-12-12 00:22:45 -0500

    CReated Observable events, WIP

--- src/app/meetup-service/meetup.service.ts
+++ src/app/meetup-service/meetup.service.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestoreDocument, AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Event } from './meetup-service.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetupService {
-
-  private eventsCollection: AngularFirestoreCollection<Event>;
-  public events: Observable<Event[]>;
-
   constructor(private db: AngularFirestore) {
-    db.collection('meetup-events').ref.onSnapshot(function (querySnapshot) {
-      const events = [];
-      querySnapshot.forEach(function (doc) {
-        events.push(<Event>doc.data());
-      });
-      this.events = events;
-      // console.log('Events: ' + events);
-    });
   }
 
-  getUpcomingEvents() {
-    return this.events;
+  getUpcomingEvents(): Observable<Event[]> {
+    return this.getData().snapshotChanges().pipe(map(item => {
+      const events: Event[] = [];
+      item.map(a => {
+        const data = a.payload.doc.data() as Event;
+        data['$key'] = a.payload.doc.id;
+        events.push(data as Event);
+      });
+
+      return events;
+    }));
   }
+
+  private getData() {
+    return this.db.collection<Event>('meetup-events');
+  }
+
 }
