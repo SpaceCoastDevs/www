9364fc2283226b09a307f210957935184050904e
gilcreque <me@gilcreque.com>
2020-02-27 21:21:56 -0500

    Fixed missing properties, switched to router link instead of hrefs

--- src/app/meetup-service/meetup-service.model.ts
+++ src/app/meetup-service/meetup-service.model.ts
@@ -1,45 +1,50 @@
+export interface Photo {
+  photo_link?: string;
+}
 export interface Group {
-    joinMode?: string;
-    created?:  number;
-    name?:     string;
-    groupLon?: number;
-    id?:       number;
-    urlname?:  string;
-    groupLat?: number;
-    who?:      string;
+  joinMode?: string;
+  created?: number;
+  name?: string;
+  groupLon?: number;
+  id?: number;
+  urlname?: string;
+  groupLat?: number;
+  who?: string;
+  group_photo?: Photo;
 }
 
 export interface Venue {
-    country?:              string;
-    localizedCountryName?: string;
-    city?:                 string;
-    address1?:             string;
-    name?:                 string;
-    lon?:                  number;
-    id?:                   number;
-    state?:                string;
-    lat?:                  number;
-    repinned?:             boolean;
+  country?: string;
+  localizedCountryName?: string;
+  city?: string;
+  address1?: string;
+  name?: string;
+  lon?: number;
+  id?: number;
+  state?: string;
+  lat?: number;
+  repinned?: boolean;
 }
 
 export interface Event {
-    utcOffset?:      number;
-    venue?:          Venue;
-    rsvpLimit?:      number;
-    headcount?:      number;
-    visibility?:     string;
-    waitlistCount?:  number;
-    created?:        number;
-    maybeRsvpCount?: number;
-    description?:    string;
-    eventURL?:       string;
-    yesRsvpCount?:   number;
-    duration?:       number;
-    name?:           string;
-    id?:             string;
-    photoURL?:       string;
-    time?:           number;
-    updated?:        number;
-    group?:          Group;
-    status?:         string;
+  utcOffset?: number;
+  venue?: Venue;
+  rsvpLimit?: number;
+  headcount?: number;
+  visibility?: string;
+  waitlistCount?: number;
+  created?: number;
+  maybeRsvpCount?: number;
+  description?: string;
+  eventURL?: string;
+  yesRsvpCount?: number;
+  duration?: number;
+  name?: string;
+  id?: string;
+  photoURL?: string;
+  time?: number;
+  updated?: number;
+  group?: Group;
+  status?: string;
+  featured_photo?: Photo;
 }
